4e8e7080de06c8bd4d543b4374145e0c2b6f8b53
hosseinvalikhani <hosseinv.dev@gmail.com>
2024-09-08 21:54:53 +0330

    project started

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Props {
+  url: string;
+  Name: string;
+  description: string;
+}
+const mobileData = [];
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold">Hello world!</h1>
+  <img src="./../assets/img/image--1.webp" alt="" />
+</template>
+
+<style scoped></style>
